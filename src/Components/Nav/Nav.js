8f76ddaf616c8ed0bc6aa27c84df8163704fa2ea
mando98 <mando1621998@gmail.com>
2021-01-02 00:53:50 +0200

    first

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Container,Navbar} from 'react-bootstrap'
+
+const Nav = () => {
+    return(
+        <Container>
+            <Navbar expand="lg" variant="light" bg="light">
+                <Navbar.Brand href="#">Navbar</Navbar.Brand>
+
+            
+
+            </Navbar>
+        </Container>
+
+    )
+}
+
+export default Nav
