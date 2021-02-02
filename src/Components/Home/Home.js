84f04ff661b2e055f91d016a991499fa42b0f9ce
Esraa-Hareedy <esraahareedy80@gmail.com>
2021-02-02 21:27:12 +0200

    first commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
 import './Home.css'
 import {Container} from 'react-bootstrap'
+import logo from "../img/aaaaa.png"
 class Home extends Component{
     render(){
         return(
-            <Container>
-                <div>
-                <h1>Hello</h1>
+            
+            <div class="main">
+                <div class="container d-flex h-100 flex-column justify-content-center align-items-center">
+                    <h1> أختــار حـرفتـكـ أو شطـب شقيتـكـ</h1>
+                    <img src={logo} width='150px' alt="" />
+                    <button class="btn btn-dark">خدمتنا</button>
                 </div>
-            </Container>
+                <div class="wave"></div>
+            </div>          
+                   
+       
+
         )
     }
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import '../NavBar/NavBar.css'
-import logo from "../img/logo.png"
-import {Container,Nav,Navbar,NavDropdown} from 'react-bootstrap'
+import logo from "../img/aaaaa.png"
+import {Container,Nav,Navbar,NavDropdown,Row,Col} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 function NavBar() {
     return(
         
         <Navbar className="bg-color" expand="lg" variant="dark">
             <Container>
-                <Navbar.Brand><Link to='/'><span>صنايعي خبره<img src={logo} width='100px' alt="" /></span></Link></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link><Link to='/'>الرئيسية</Link></Nav.Link>
-                    <Nav.Link><Link to='/services'>خدمتنا</Link></Nav.Link>
-                    <NavDropdown title="سجل الان" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1"><Link to='/worker'>صنايعي</Link></NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4"><Link to='/customer'>عميل</Link></NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                </Navbar.Collapse>
+                <Row className="NavRow">
+                    <Col lg={6} md={6} sm={6}>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ml-auto">
+                                <Nav.Link><Link to='/'>الرئيسية</Link></Nav.Link>
+                                <Nav.Link><Link to='/services'>خدمتنا</Link></Nav.Link>
+                                <NavDropdown title="سجل الان" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1"><Link to='/worker'>صنايعي</Link></NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4"><Link to='/customer'>عميل</Link></NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Col>
+                    <Col lg={6} md={6} sm={6}>
+                        <Navbar.Brand><Link to='/'><span>صنايعي خبره<img src={logo} width='90px' alt="" /></span></Link></Navbar.Brand>
+                    </Col>
+                </Row>
             </Container>
         </Navbar>
         
