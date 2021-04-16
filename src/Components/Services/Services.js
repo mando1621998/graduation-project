bd08228410ca1ee0ad6e8f79f69f2e7f33645207
mando98 <mando1621998@gmail.com>
2021-04-16 17:20:30 +0200

    Services Page First Change

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,13 +1,46 @@
-import React, { Component } from 'react'
+import Axios from 'axios'
+import React , { Component }from 'react'
+import './Services.css'
 
-class Services extends Component {
-    render() {
-        return (
-            <div>
-                Services
+class Services extends Component{
+    
+    state = {
+        works:[]
+    }
+    componentDidMount = () => {
+        Axios.get('js/data.json').then(res => {this.setState({works: res.data.works})})
+    }
+
+    render(){
+
+        const worksList = this.state.works.map( (workItem) =>{
+            return(
+                        <React.Fragment>
+                            <div className="product">
+                                <div className="imgbox">
+                                    <img src={workItem.image_body} alt="watch" />
+                                </div>
+                                <div className="content">
+                                    <h2>{workItem.title}</h2><br />
+                                    
+                                <a href="#">اطلب الان</a>
+                                </div>
+                            </div>
+                        </React.Fragment>
+                   
+            )
+        })
+    
+        return(
+            <div className="work">
+                <div className="container">
+                    
+                    {worksList}
+                </div>
             </div>
         )
     }
+    
+    
 }
-
 export default Services
